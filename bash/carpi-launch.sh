d7f27ef187b638dabcc014ecabfa93f4f4192006
Richard Denton <isdampe@gmail.com>
2014-10-04 11:35:19 +0000

    Comments

--- bash/carpi-launch.sh
+++ bash/carpi-launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #This should launch automatically as root on boot.
+#You can do this by adding the following to /etc/rc.local
+#
+#sudo bash ~/carpi/bash/carpi-launch.sh &
 
 pkill gpsd
 sleep 1
